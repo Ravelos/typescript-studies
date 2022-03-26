ea4834b9bfd4352aabd31c3c839ccbcbb966525f
Oswaldo Castillo <oswaldo.castillo@iteris.com.br>
2022-03-26 12:54:58 -0300

    trying with js but it is not working

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,1 +1,6 @@
-alert('Hello');
+import { Negociacao } from "./models/negociacao";
+
+const negociacao = new Negociacao(new Date(),10, 1000);
+console.log(negociacao);
+negociacao.quantidade = 10000;
+console.log(negociacao);
---
+++ dist/js/models/negociacao.js
@@ -0,0 +1,11 @@
+export class Negociacao{
+    #data:
+    #quantidade;
+    #valor;
+
+    constructor(){
+        this.#data = data;
+        this.#quantidade = quantidade;
+        this.#valor = valor;
+    }
+}
